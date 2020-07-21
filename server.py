09518aa09046c768db496557a150f2cb05b7ea56
Rapcis <Rapcis@users.noreply.github.com>
2020-07-21 18:16:33 +0100

    my prtfolio website files

---
+++ server.py
@@ -0,0 +1,46 @@
+from flask import Flask, escape, request, render_template, url_for, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def my_home(page_name):
+    return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return redirect('thank_you.html')
+        except:
+            return 'problem with database'
+    else:
+        return 'something went wrong'
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as database_2:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database_2, delimiter=',',
+                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
